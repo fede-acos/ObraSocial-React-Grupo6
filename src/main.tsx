b0752b52aa1cd76ca99d9cfb345de97f293f36e5
NicoBrites <nicolas.britesv@gmail.com>
2024-06-29 16:06:44 -0300

    Agregado de page de Especialistas

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Providers from "./providers/Providers.tsx";
-import ListaEspecialistas from "./modules/listaEspecialistas.tsx";
+import CartillaMedica from "./pages/cartillaMedica.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
-      <ListaEspecialistas />
+      <CartillaMedica />
       <App />
     </Providers>
   </React.StrictMode>
---
+++ src/pages/cartillaMedica.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ListaEspecialistas from '../modules/listaEspecialistas';
+
+
+const CartillaMedica: React.FC = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen py-8">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">Cartilla Médica</h1>
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        <div className="p-6">
+          <ListaEspecialistas />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartillaMedica;
