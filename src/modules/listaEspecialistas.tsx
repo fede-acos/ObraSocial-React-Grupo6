75767abfaee5b081a149fa192516117f1e7ecd05
NicoBrites <nicolas.britesv@gmail.com>
2024-06-29 15:55:21 -0300

    Agregado de cards responsive y avatar random

--- src/modules/listaEspecialistas.tsx
+++ src/modules/listaEspecialistas.tsx
@@ -12,6 +12,7 @@ const ListaEspecialistas: React.FC = () => {
       try {
         const data = await fetchData('/especialistas');
         setData(data);
+
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -26,13 +27,13 @@ const ListaEspecialistas: React.FC = () => {
     return <p>Loading...</p>;// agregar circular progress
   }
 
-  return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+  return ( // falta cambiar avatar
+    <div className="p-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
     {data.map((item, index) => (
-       <Card className="max-w-[340px]">
+        <Card key={index} className="m-2 w-full max-w-xs">
        <CardHeader className="justify-between">
          <div className="flex gap-5">
-           <Avatar isBordered radius="full" size="md" src="https://nextui.org/avatars/avatar-1.png" />
+           <Avatar isBordered radius="full" size="md" src="https://xsgames.co/randomusers/avatar.php?g=male" /> 
            <div className="flex flex-col gap-1 items-start justify-center">
              <h4 className="text-small font-semibold leading-none text-default-600">{item.nombre}</h4>
              <h5 className="text-small tracking-tight text-default-400">{item.especialidad}</h5>
@@ -47,19 +48,19 @@ const ListaEspecialistas: React.FC = () => {
             Reservar turno
          </Button>
        </CardHeader>
-       <CardBody className="px-3 py-0 text-small text-default-400">
+       <CardBody className="px-3 py-0 text-small text-default-600">
          <p>
            {item.ubicacion.provincia} - {item.ubicacion.ciudad}
          </p>
        </CardBody>
        <CardFooter className="gap-3">
          <div className="flex gap-1">
-           <p className="font-semibold text-default-400 text-small">Horario Entrada:</p>
-           <p className=" text-default-400 text-small">{item.horarioEntrada}</p>
+           <p className="font-semibold text-default-600 text-small">Horario Entrada:</p>
+           <p className=" text-default-600 text-small">{item.horarioEntrada}</p>
          </div>
          <div className="flex gap-1">
-           <p className="font-semibold text-default-400 text-small">Horario Salida:</p>
-           <p className="text-default-400 text-small">{item.horarioSalida}</p>
+           <p className="font-semibold text-default-600 text-small">Horario Salida:</p>
+           <p className="text-default-600 text-small">{item.horarioSalida}</p>
          </div>
        </CardFooter>
      </Card>
