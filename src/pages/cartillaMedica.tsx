1ef5ae6a5c9f434379d3909bd87b4bdc78800fdb
NicoBrites <nicolas.britesv@gmail.com>
2024-06-29 16:16:14 -0300

    Agregado de Tailwind a page Cartilla Medica

--- src/pages/cartillaMedica.tsx
+++ src/pages/cartillaMedica.tsx
@@ -4,7 +4,7 @@ import ListaEspecialistas from '../modules/listaEspecialistas';
 
 const CartillaMedica: React.FC = () => {
   return (
-    <div className="bg-gray-100 min-h-screen py-8">
+    <div className="flex flex-col items-center min-h-screen bg-gray-100 py-8">
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">Cartilla Médica</h1>
       <div className="bg-white rounded-lg shadow-lg overflow-hidden">
         <div className="p-6">
