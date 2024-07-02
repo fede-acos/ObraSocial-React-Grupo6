8cc618a369ef339d1ad26e473427a3cb27911b45
Federico Acosta <fede.acosta2396@gmail.com>
2024-07-01 21:01:12 -0300

    se agrego interceptors para cuando se hacen request y errores

---
+++ src/config/axiosConfig.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+const baseUrl = "http://localhost:8080/";
+
+export const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.reload();
+      // hace un reload si el token no es valido para que te redireccione a login
+    }
+    return Promise.reject(error);
+  }
+);
