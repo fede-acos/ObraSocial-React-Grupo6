1b71c96407151befd8d33c01310c3eb008a77081
NicoBrites <nicolas.britesv@gmail.com>
2024-07-03 17:22:23 -0300

    Agregado de react-hook-form

--- src/modules/turnosForm.tsx
+++ src/modules/turnosForm.tsx
@@ -1,3 +1,4 @@
+import {useForm, Controller, SubmitHandler } from "react-hook-form"
 import {Calendar, DateValue} from "@nextui-org/react";
 import { SpecialistDto } from "../types/SpecialistDto";
 import { isWeekend} from "@internationalized/date";
@@ -7,22 +8,60 @@ interface FormTurnosProps {
     specialist: SpecialistDto;
 }
 
+interface FormValues  {
+    motivoConsulta: string
+    selectedDate: Date | null
+    selectedButtonTime : string | null
+}
+
 function TurnosForm ({ specialist }: FormTurnosProps) {
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: { // pacienteid, especialistaId, fecha, hora, motivoConsulta
+        motivoConsulta: "",
+        selectedDate: null,
+        selectedButtonTime: null
+        },
+    })
     
     const isDateUnavailable = (date : DateValue) =>
         isWeekend(date, locale) ;
+
     const {locale} = useLocale();
     
-    return (
-        
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log(data)
+    }
 
-        <Calendar
+
+
+
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="form-container">
+        <div className="form-block">
+            <h1>{specialist.nombre}</h1>
+        </div>
+
+        <div className="form-block">
+            <h2><b>Seleccione fecha y hora del turno</b></h2>
+            <Controller
+            name="selectedDate"
+            control={control}
+            rules={{ required: "pedilo" }}
+            render={({ field }) => (
+            <Calendar
                 calendarWidth ="300px"
                 className="custom-calendar"
                 aria-label="Date (Unavailable)"
                 isDateUnavailable={isDateUnavailable}
-                
+                onChange={(date) => field.onChange(date)}
+                value={field.value}
             />
+            )}
+        />
+        <p className="error">{errors.selectedDate?.message}</p>
+        </div>
+        </form>
     )
 }
 
