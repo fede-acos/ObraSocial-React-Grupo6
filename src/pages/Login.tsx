bc85fa9f4a25b66a063041baf7058186c74fb63a
DavidBernhardt <59166507+DavidBernhardt@users.noreply.github.com>
2024-07-03 18:44:14 -0300

    less input width

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -16,7 +16,7 @@ function Login() {
       <div className="hidden w-1/2 lg:block">
         <img src="src/assets/image/promo-image.jpg" alt="Promo image" />
       </div>
-      <div className="w-full md:w-1/2 lg:w-1/2 lg:ml-0 ml-20 mr-20 flex flex-col allign-center">
+      <div className="w-full md:w-1/3 lg:w-1/3 lg:ml-0 ml-20 mr-20 flex flex-col allign-center">
         <div className="flex flex-col items-center">
           <h1 className="text-3xl font-bold">Acceder</h1>
           <p>¿No tienes cuenta? <Link href="/signup">Crea una</Link></p>
